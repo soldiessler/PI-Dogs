bdefe29a29d40bcaaf9ab9cca1efbced1e9ccc17
Sol Diessler <sol.diessler@gmail.com>
2023-02-20 22:12:10 -0300

    responsive

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -4,7 +4,9 @@ import style from "./Footer.module.css"
 export default function Footer(){
   return(
     <div className={style.footer}> 
-     <p className={style.footer_text}>Page by Sol Diessler</p>
+     <p className={style.footer_text}>Made by 
+     <a href="https://github.com/soldiessler" className={style.footer_link}> Sol Diessler
+     </a> </p>
     </div>
   )
 }
--- client/src/components/landing/LandingPage.jsx
+++ client/src/components/landing/LandingPage.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import style from "./LandingPage.module.css"
-import doggo from "../../assets/doggo.svg"
+import doggo from "../../assets/doggo.png"
 
 export default function LandingPage () {
   return (
-    <div> 
-      <div>
-        <h1 className={style.brand_title}>DOGS</h1>
-        <p className={style.brand_desc}>We're here to help you find <br /> the breed that best suits you and <br /> your lifestyle!</p>
-          <Link to='/home'>
-            <button className={style.brand_btn}>Let's go</button>
-          </Link>
-      </div>
+    <div className={style.landing_container}> 
+        <div className={style.landing_text}>
+          <h1 className={style.brand_title}>DOGS</h1>
+          <p className={style.brand_desc}>We're here to help you find <br /> the breed that best suits you and <br /> your lifestyle!</p>
+            <Link to='/home'>
+              <button className={style.brand_btn}>Let's go</button>
+            </Link>
+        </div>
       <img className={style.img} src={doggo} alt="doggo"></img>
     </div>
   )
